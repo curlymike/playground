478a6792c702c633ba089e941c70641433a6d05c
Mike <curlymike@gmail.com>
2019-07-07 14:03:57 +0300

    Annotation configuration - default bean scope

--- src/main/java/ru/innopolis/stc16/Launcher.java
+++ src/main/java/ru/innopolis/stc16/Launcher.java
@@ -7,7 +7,10 @@ import ru.innopolis.stc16.entity.Person;
 public class Launcher {
   public static void main(String[] args) {
     ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
-    Person person = context.getBean(Person.class);
-    System.out.println(String.valueOf(person));
+    Person person1 = context.getBean(Person.class);
+    Person person2 = context.getBean(Person.class);
+    System.out.println(String.valueOf(person1));
+    System.out.println(String.valueOf(person2));
+    System.out.println(person1 == person2);
   }
 }
